daf920cf7b56c2986bfc1630cf021b481e77e58f
Lorena Sarasua <lorena.sarasua@gmail.com>
2022-06-12 00:55:05 -0400

    created rooms and weapons objects. added to respective arrays

--- src/clue.js
+++ src/clue.js
@@ -66,16 +66,100 @@ const mrMustard = {
 
 suspectsArray.push(mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard);
 
-// console.log(suspectsArray);
-
 // Rooms Array
 
 const roomsArray = [];
+
+const diningRoom = {
+  name: "Dining Room"
+}
+const conservatory = {
+  name: "Conservatory"
+}
+const kitchen = {
+  name: "Kitchen"
+}
+const study = {
+  name: "Study"
+}
+const library = {
+  name: "Library"
+}
+const billiardRoom = {
+  name: "Billiard Room"
+}
+const lounge = {
+  name: "Lounge"
+}
+const ballroom = {
+  name: "Ballroom"
+}
+const hall = {
+  name: "Hall"
+}
+const spa = {
+  name: "Spa"
+}
+const livingRoom = {
+  name: "Living Room"
+}
+const observatory = {
+  name: "Observatory"
+}
+const theater = {
+  name: "Theater"
+}
+const guestHouse = {
+  name: "Guest House"
+}
+const patio = {
+  name: "Patio"
+}
+
+roomsArray.push(diningRoom, conservatory, kitchen, study, library, billiardRoom, lounge, ballroom, hall, spa, livingRoom, observatory, theater, guestHouse, patio);
 
 // Weapons Array
 
 const weaponsArray = [];
 
+const rope = {
+  name: "rope", 
+  weight: 10
+}
+const knife = {
+  name: "knife",
+  weight: 8
+}
+const candlestick = {
+  name: "candlestick", 
+  weight: 2
+}
+const dumbbell = {
+  name: "dumbbell", 
+  weight: 30
+}  
+const poison = {
+  name: "poison", 
+  weight: 2
+}
+const axe = {
+  name: "axe", 
+  weight: 15
+}  
+const bat = {
+  name: "bat", 
+  weight: 13
+}  
+const trophy = {
+  name: "trophy", 
+  weight: 25
+}  
+const pistol = {
+  name: "pistol", 
+  weight: 20
+}  
+
+weaponsArray.push(rope, knife, candlestick, dumbbell, poison, axe, bat, trophy, pistol);
 
 // ITERATION 2
 
